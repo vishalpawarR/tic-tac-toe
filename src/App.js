4a6fb8385f6cbb6c57499240570441ad726989b0
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-06-28 13:05:55 +0530

    feat: added the click event handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { createElement, useState } from 'react';
 import './App.css';
 import Cell from './components/Cell';
 
@@ -6,13 +6,19 @@ const gridSize = 6;
 const PLAYERS = ['X', 'Y', 'O'];
 
 function App() {
+  
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [winner, setWinner] = useState('');
 
   const handleClick = (e) => {
-    console.log(e.target)
+    if(e.target.childNodes.length === 0) {
+      const span = document.createElement("span");
+      span.innerText = currentPlayer;
+      e.target.appendChild(span);
+      setCurrentPlayer(PLAYERS[(PLAYERS.indexOf(currentPlayer) + 1) % PLAYERS.length])
+    }
+    return
   }
-
-  const [currentlayer, setCurrentPlayer] = useState(PLAYERS[0]);
-  const [winner, setWinner] = useState('');
 
   const renderCells = () => {
     const cells = [];
@@ -27,7 +33,7 @@ function App() {
     <>
       <div className="flex justify-center items-center h-screen flex-col">
         <h1 className='text-2xl font-semibold'>Welcome to Tic Tac Toe</h1>
-        <p>Next player turn: {currentlayer}</p>
+        <p>Next player turn: {currentPlayer}</p>
         <div className="board-container w-[440px] h-[440px] border border-gray-400 flex flex-wrap border-box justify-center items-center">
           {renderCells()}
         </div>
